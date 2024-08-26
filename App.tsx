64cf512c153fad43c474461ad4aea2425a3f643c
Graev Konstantin <54083933+Graev@users.noreply.github.com>
2024-08-26 12:17:15 +0700

    4.13

--- App.tsx
+++ App.tsx
@@ -1,11 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { Button } from "./shared/Button/Button";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={{ gap: 24 }}>
+        <View style={{ gap: 8 }}>
+          <Text style={styles.header}>
+            Одно из самых вкусных кофе в городе!
+          </Text>
+          <Text style={styles.description}>
+            Свежие зёрна, настоящая арабика и бережная обжарка
+          </Text>
+        </View>
+        <Button label={"Начать"} onPress={() => {}} />
+      </View>
     </View>
   );
 }
@@ -13,8 +23,23 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#000",
+    alignItems: "center",
+    justifyContent: "flex-end",
+    paddingBottom: 43,
+    paddingHorizontal: 30,
   },
+  header: {
+    fontSize: 34,
+    color: "#fff",
+    textAlign: "center",
+    fontWeight: "bold",
+    marginBottom: 8,
+  },
+  description: {
+    fontSize: 14,
+    color: "#A9A9A9",
+    textAlign: "center",
+  },
+  button: {},
 });
---
+++ shared/tokens.ts
@@ -0,0 +1,4 @@
+export const Colors = {
+  primary: "#C67C4E",
+  gray: "#9B9B9B",
+};
