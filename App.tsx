9a804d03b2e0db5cf722f7054f0f9fd9ac931276
Graev Konstantin <54083933+Graev@users.noreply.github.com>
2024-09-27 17:26:46 +0300

    7 add eslint and fix

--- App.tsx
+++ App.tsx
@@ -1,99 +1,94 @@
-import {
-  Animated,
-  ImageBackground,
-  StyleSheet,
-  Text,
-  View,
-} from "react-native";
-import { Button } from "./shared/Button/Button";
-import { Fonts } from "./shared/tokens";
+import { Animated, ImageBackground, StyleSheet, Text, View } from 'react-native';
+import { Button } from './shared/Button/Button';
+import { Colors, Fonts } from './shared/tokens';
 
 const useAnimatedTextShow = () => {
-  const opacity = new Animated.Value(0);
-  const position = new Animated.Value(-75);
+	const opacity = new Animated.Value(0);
+	const position = new Animated.Value(-75);
 
-  const DURATION = 1000;
-  const USE_NATIVE_DRIVER = true;
+	const DURATION = 1000;
+	const USE_NATIVE_DRIVER = true;
 
-  const animateIn = () => {
-    Animated.timing(opacity, {
-      toValue: 1,
-      duration: DURATION,
-      useNativeDriver: USE_NATIVE_DRIVER,
-    }).start();
-    Animated.timing(position, {
-      toValue: 0,
-      duration: DURATION,
-      useNativeDriver: USE_NATIVE_DRIVER,
-    }).start();
-  };
+	const animateIn = () => {
+		Animated.timing(opacity, {
+			toValue: 1,
+			duration: DURATION,
+			useNativeDriver: USE_NATIVE_DRIVER,
+		}).start();
+		Animated.timing(position, {
+			toValue: 0,
+			duration: DURATION,
+			useNativeDriver: USE_NATIVE_DRIVER,
+		}).start();
+	};
 
-  return {
-    opacity,
-    position,
-    animateIn,
-  };
+	return {
+		opacity,
+		position,
+		animateIn,
+	};
 };
 
 export default function App() {
-  const { position, opacity, animateIn } = useAnimatedTextShow();
+	const { position, opacity, animateIn } = useAnimatedTextShow();
 
-  return (
-    <View style={styles.container}>
-      <ImageBackground
-        style={styles.background}
-        source={require("./assets/welcome-background.png")}
-        imageStyle={{
-          resizeMode: "contain",
-          top: -166,
-        }}
-      >
-        <Animated.View
-          style={{
-            transform: [{ translateY: position }],
-            opacity,
-          }}
-          onLayout={animateIn}
-        >
-          <Text style={styles.header}>
-            Одно из самых вкусных кофе в городе!
-          </Text>
-        </Animated.View>
-        <View style={{ gap: 24 }}>
-          <Text style={styles.description}>
-            Свежие зёрна, настоящая арабика и бережная обжарка
-          </Text>
+	return (
+		<View style={styles.container}>
+			<ImageBackground
+				style={styles.background}
+				//eslint-disable-next-line @typescript-eslint/no-require-imports
+				source={require('./assets/welcome-background.png')}
+				imageStyle={styles.image}
+			>
+				<Animated.View
+					style={{
+						transform: [{ translateY: position }],
+						opacity,
+					}}
+					onLayout={animateIn}
+				>
+					<Text style={styles.header}>Одно из самых вкусных кофе в городе!</Text>
+				</Animated.View>
+				<View style={styles.gap}>
+					<Text style={styles.description}>Свежие зёрна, настоящая арабика и бережная обжарка</Text>
 
-          <Button title={"Начать"} onPress={() => {}} />
-        </View>
-      </ImageBackground>
-    </View>
-  );
+					<Button title={'Начать'} onPress={() => {}} />
+				</View>
+			</ImageBackground>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#000",
-    paddingBottom: 43,
-  },
-  background: {
-    flex: 1,
-    gap: 8,
-    alignItems: "center",
-    justifyContent: "flex-end",
-    paddingHorizontal: 30,
-  },
-  header: {
-    fontSize: Fonts.f34,
-    color: "#fff",
-    textAlign: "center",
-    fontWeight: "bold",
-    marginBottom: 8,
-  },
-  description: {
-    fontSize: Fonts.f14,
-    color: "#A9A9A9",
-    textAlign: "center",
-  },
+	container: {
+		flex: 1,
+		backgroundColor: Colors.black,
+		paddingBottom: 43,
+	},
+	background: {
+		flex: 1,
+		gap: 8,
+		alignItems: 'center',
+		justifyContent: 'flex-end',
+		paddingHorizontal: 30,
+	},
+	header: {
+		fontSize: Fonts.f34,
+		color: Colors.white,
+		textAlign: 'center',
+		fontWeight: 'bold',
+		marginBottom: 8,
+	},
+	description: {
+		fontSize: Fonts.f14,
+		color: Colors.gray,
+		textAlign: 'center',
+	},
+	image: {
+		resizeMode: 'contain',
+		top: -166,
+	},
+	gap: {
+		gap: 24,
+	},
 });
--- shared/Button/Button.tsx
+++ shared/Button/Button.tsx
@@ -1,89 +1,89 @@
 import {
-  Animated,
-  GestureResponderEvent,
-  Pressable,
-  PressableProps,
-  StyleSheet,
-  Text,
-} from "react-native";
-import { Colors, Fonts, Radius } from "../tokens";
+	Animated,
+	GestureResponderEvent,
+	Pressable,
+	PressableProps,
+	StyleSheet,
+	Text,
+} from 'react-native';
+import { Colors, Fonts, Radius } from '../tokens';
 
 type ButtonProps = {
-  title: string;
+	title: string;
 } & PressableProps;
 
 const useAnimatedButton = () => {
-  const animatedValue = new Animated.Value(0);
-  const color = animatedValue.interpolate({
-    inputRange: [0, 100],
-    outputRange: [Colors.primary, Colors.primaryHover],
-  });
+	const animatedValue = new Animated.Value(0);
+	const color = animatedValue.interpolate({
+		inputRange: [0, 100],
+		outputRange: [Colors.primary, Colors.primaryHover],
+	});
 
-  const DURATION = 200;
-  const USE_NATIVE_DRIVER = true;
+	const DURATION = 200;
+	const USE_NATIVE_DRIVER = true;
 
-  const animateIn = () => {
-    Animated.timing(animatedValue, {
-      toValue: 100,
-      duration: DURATION,
-      useNativeDriver: USE_NATIVE_DRIVER,
-    }).start();
-  };
+	const animateIn = () => {
+		Animated.timing(animatedValue, {
+			toValue: 100,
+			duration: DURATION,
+			useNativeDriver: USE_NATIVE_DRIVER,
+		}).start();
+	};
 
-  const animateOut = () => {
-    Animated.timing(animatedValue, {
-      toValue: 0,
-      duration: DURATION,
-      useNativeDriver: USE_NATIVE_DRIVER,
-    }).start();
-  };
+	const animateOut = () => {
+		Animated.timing(animatedValue, {
+			toValue: 0,
+			duration: DURATION,
+			useNativeDriver: USE_NATIVE_DRIVER,
+		}).start();
+	};
 
-  return {
-    color,
-    animateIn,
-    animateOut,
-  };
+	return {
+		color,
+		animateIn,
+		animateOut,
+	};
 };
 
 export const Button = ({ title, ...props }: ButtonProps) => {
-  const style = StyleSheet.compose(
-    styles.button,
-    typeof props.style !== "function" ? props.style : {},
-  );
+	const style = StyleSheet.compose(
+		styles.button,
+		typeof props.style !== 'function' ? props.style : {},
+	);
 
-  const { color, animateOut, animateIn } = useAnimatedButton();
+	const { color, animateOut, animateIn } = useAnimatedButton();
 
-  const handlePressIn = (e: GestureResponderEvent) => {
-    animateIn();
-    props.onPressIn && props.onPressIn(e);
-  };
+	const handlePressIn = (e: GestureResponderEvent) => {
+		animateIn();
+		props.onPressIn && props.onPressIn(e);
+	};
 
-  const handlePressOut = (e: GestureResponderEvent) => {
-    animateOut();
-    props.onPressOut && props.onPressOut(e);
-  };
+	const handlePressOut = (e: GestureResponderEvent) => {
+		animateOut();
+		props.onPressOut && props.onPressOut(e);
+	};
 
-  return (
-    <Pressable {...props} onPressIn={handlePressIn} onPressOut={handlePressOut}>
-      <Animated.View style={[style, { backgroundColor: color }]}>
-        <Text style={styles.text}>{title}</Text>
-      </Animated.View>
-    </Pressable>
-  );
+	return (
+		<Pressable {...props} onPressIn={handlePressIn} onPressOut={handlePressOut}>
+			<Animated.View style={[style, { backgroundColor: color }]}>
+				<Text style={styles.text}>{title}</Text>
+			</Animated.View>
+		</Pressable>
+	);
 };
 
 const styles = StyleSheet.create({
-  button: {
-    backgroundColor: Colors.primary,
-    justifyContent: "center",
-    alignItems: "center",
-    paddingVertical: 21,
-    borderRadius: Radius.r16,
-  },
-  text: {
-    color: "#fff",
-    textAlign: "center",
-    fontWeight: "bold",
-    fontSize: Fonts.f16,
-  },
+	button: {
+		backgroundColor: Colors.primary,
+		justifyContent: 'center',
+		alignItems: 'center',
+		paddingVertical: 21,
+		borderRadius: Radius.r16,
+	},
+	text: {
+		color: Colors.white,
+		textAlign: 'center',
+		fontWeight: 'bold',
+		fontSize: Fonts.f16,
+	},
 });
